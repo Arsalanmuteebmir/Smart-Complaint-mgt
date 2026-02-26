f21ef11586c5f9fce1ca9f412c722c259bd0617b
Arsalan Muteeb Mir <arsalanmir29@gmail.com>
2026-02-26 20:28:20 +0530

    fix cors for vercel frontend

--- server/server.js
+++ server/server.js
@@ -8,12 +8,21 @@ import complaintRoutes from "./routes/complaintRoutes.js";
 dotenv.config();
 const app = express();
 connectDB();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://smart-complaint-mgt-new.vercel.app"
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://smart-complaint-mgt-new.vercel.app"
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
